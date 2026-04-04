6ba9ceb11ba85b021ee838ad2356ab88c01c529a
MarcelFeo <marcelfeo29@gmail.com>
2026-04-04 14:10:05 -0300

    start project

---
+++ src/main/java/com/example/priceflow/PriceflowApplication.java
@@ -0,0 +1,13 @@
+package com.example.priceflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/priceflow/PriceflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.priceflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
